9b38c9a9a2361a7c6723284d539530dd3aba038a
CamilaRCM <101403356+CamilaRCM@users.noreply.github.com>
2022-09-29 18:18:26 -0300

    Add files via upload

---
+++ Exercicio 2/FPadrao.java
@@ -0,0 +1,13 @@
+package br.com.POO2;
+
+public class FPadrao extends Funcionario{
+
+  public FPadrao(int matricula, String nome, double salario) {
+    super(matricula, nome, salario);    
+  }
+
+  @Override
+  public double calcularProventos() {    
+    return getSalario();
+  }
+}
